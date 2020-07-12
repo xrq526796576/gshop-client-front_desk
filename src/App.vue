ea7e7ad69a3e2554c5d08ec5400a5b3bbd781d63
xurenqiang <526796576@qq.com>
2020-07-12 17:52:45 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="outer">
+    <!-- 头部 -->
+    <Header></Header>
+    <!-- 商品分类导航 -->
+    <router-view></router-view>
+    <!-- 底部 -->
+    <Footer></Footer>
+  </div>
+</template>
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+</style>
---
+++ src/pages/Search/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Search
+    <br />
+    <!-- params:{{this.$route.params.keyword}}
+    query:{{this.$route.query.keyword1}}-->
+    params:{{keyword}}
+    query:{{keyword1}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'Search',
+  props: ['keyword', 'keyword1']
+}
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+</style>
